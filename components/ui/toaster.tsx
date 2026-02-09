4f0e075b1abd8b2362b7a4e37375182b777b42af
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-02-09 17:35:24 +0530

    add components, proxy

---
+++ components/ui/toaster.tsx
@@ -0,0 +1,16 @@
+"use client";
+import * as React from "react";
+import { Toaster as Sonner } from "sonner";
+
+type ToasterProps = React.ComponentProps<typeof Sonner>;
+
+export function Toaster({ ...props }: ToasterProps) {
+  return (
+    <Sonner
+      position="top-right"
+      richColors
+      closeButton
+      {...props}
+    />
+  );
+}
