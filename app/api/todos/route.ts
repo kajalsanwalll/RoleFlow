66a8078e6db70d784e7414d818309dea60d44a8e
Kajal Sanwal <kajalsanwal777@gmail.com>
2026-02-13 19:14:53 +0530

    GET POST functions written for todo

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@clerk/nextjs/server";
 import prisma from "@/lib/prisma";
+import { error } from "console";
 
 const ITEMS_PER_PAGE = 10
 
@@ -60,10 +61,39 @@ export async function GET(req: NextRequest){
 }
 
 
-export async function POST(){
+export async function POST(req:NextRequest){
   const { userId} = await auth();
 
     if(!userId){
         return NextResponse.json({error:"Unauthorized!"}, {status:400})
     }
+
+    const user = await prisma.user.findUnique({
+        where:{id: userId},
+        include:{todos: true}
+    })
+
+    console.log(user);
+
+    if(!user){
+        return NextResponse.json({error:"User not found!"},{status:404})
+    }
+
+    if(!user.isSubscribed && user.todos.length >=3){
+        return NextResponse.json({
+            error: "Free users can only create upto 3 todos. Please subscribe to our paid plans to write more awesome todos."
+
+        },
+        {
+            status:404
+        })
+    }
+
+    const {title} =await req.json();
+    const todo = await prisma.todo.create({
+        data: {title, userId}
+    })
+
+    return NextResponse.json(todo, {status:201})
+    
 }
